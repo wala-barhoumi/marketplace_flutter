456a5924975c7750b3981101676a5857ec3188e6
wala-barhoumi <walabarhoumi081@gmail.com>
2024-12-23 17:24:47 +0100

    orders

--- lib/dashboard/orders_page.dart
+++ lib/dashboard/orders_page.dart
@@ -15,159 +15,20 @@ class _OrdersPageState extends State<OrdersPage> {
   Stream<List<Map<String, dynamic>>> getOrdersStream() {
     return _firestore.collection('orders').snapshots().asyncMap((querySnapshot) async {
       List<Map<String, dynamic>> orders = [];
-      for (var doc in querySnapshot.docs) {
-        var orderData = doc.data();
-        try {
-          var subCollection = await doc.reference.collection('order_details').get();
-          orderData['order_details'] = subCollection.docs.map((detail) => detail.data()).toList();
-        } catch (e) {
-          orderData['order_details'] = [];
-          debugPrint('Error fetching order details for ${doc.id}: $e');
-        }
-        orderData['id'] = doc.id;
-        orders.add(orderData);
-      }
+
       return orders;
     });
   }
-
-  // Fetch Products Stream
-  Stream<List<Map<String, dynamic>>> getProductsStream() {
-    return _firestore.collection('products').snapshots().map((querySnapshot) {
-      return querySnapshot.docs.map((doc) {
-        return {
-          'id': doc.id,
-          'name': doc['name'],
-          'price': doc['price'],
-        };
-      }).toList();
-    });
-  }
-
-  // Add Order
-  Future<void> addOrder(Map<String, dynamic> newOrder, List<Map<String, dynamic>> orderDetails) async {
-    try {
-      DocumentReference orderRef = await _firestore.collection('orders').add(newOrder);
-      for (var detail in orderDetails) {
-        await orderRef.collection('order_details').add(detail);
-      }
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Order added successfully')),
-      );
-    } catch (e) {
-      debugPrint('Error adding order: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Error adding order')),
-      );
-    }
-  }
-
-  // Edit Order
-  Future<void> editOrder(String orderId, Map<String, dynamic> updatedData) async {
-    try {
-      await _firestore.collection('orders').doc(orderId).update(updatedData);
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Order updated successfully')),
-      );
-    } catch (e) {
-      debugPrint('Error updating order: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Error updating order')),
-      );
-    }
-  }
-
-  // Delete Order
-  Future<void> deleteOrder(String orderId) async {
-    try {
-      await _firestore.collection('orders').doc(orderId).delete();
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Order deleted successfully')),
-      );
-    } catch (e) {
-      debugPrint('Error deleting order: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Error deleting order')),
-      );
-    }
-  }
-
-  // Show Add Order Dialog
-  void showAddOrderDialog() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        List<Map<String, dynamic>> selectedProducts = [];
-        return AlertDialog(
-          title: const Text('Add Order'),
-          content: StreamBuilder<List<Map<String, dynamic>>>(
-            stream: getProductsStream(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const CircularProgressIndicator();
-              }
-              final products = snapshot.data!;
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: products.map((product) {
-                  return CheckboxListTile(
-                    title: Text(product['name']),
-                    subtitle: Text('Price: ${product['price']}'),
-                    value: selectedProducts.contains(product),
-                    onChanged: (selected) {
-                      setState(() {
-                        if (selected == true) {
-                          selectedProducts.add(product);
-                        } else {
-                          selectedProducts.remove(product);
-                        }
-                      });
-                    },
-                  );
-                }).toList(),
-              );
-            },
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await addOrder(
-                  {
-                    'order_date': DateTime.now().toString(),
-                    'status': 'Pending',
-                    'total': selectedProducts.fold<double>(0.0, (sum, item) => sum + (item['price'] as double)),
-                  },
-                  selectedProducts.map((e) => {'unit_price': e['price'], 'quantity': 1}).toList(),
-                );
-                Navigator.pop(context);
-              },
-              child: const Text('Add Order'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 189, 167, 204),
         title: const Text('Orders Dashboard'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: showAddOrderDialog,
-          ),
-        ],
       ),
-      body: StreamBuilder<List<Map<String, dynamic>>>(
-        stream: getOrdersStream(),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('orders').snapshots(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -177,54 +38,62 @@ class _OrdersPageState extends State<OrdersPage> {
             return Center(child: Text('Error: ${snapshot.error}'));
           }
 
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
             return const Center(child: Text('No orders found'));
           }
 
-          List<Map<String, dynamic>> orders = snapshot.data!;
+         var orders = snapshot.data!.docs;
 
-          return SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: DataTable(
-              columnSpacing: 16.0,
-              headingRowHeight: 56.0,
-              columns: const <DataColumn>[
-                DataColumn(label: Text('Order Date')),
-                DataColumn(label: Text('Status')),
-                DataColumn(label: Text('Total')),
-                DataColumn(label: Text('Details')),
-                DataColumn(label: Text('Actions')),
-              ],
-              rows: orders.map((order) {
-                return DataRow(
-                  cells: <DataCell>[
-                    DataCell(Text(order['order_date'] ?? 'No Date')),
-                    DataCell(Text(order['status'] ?? 'Pending')),
-                    DataCell(Text(order['total']?.toString() ?? '0')),
-                    DataCell(
-                      Text(order['order_details']
-                          .map((detail) => "Unit: ${detail['unit_price']}, Qty: ${detail['quantity']}")
-                          .join(', ')),
-                    ),
-                    DataCell(
-                      Row(
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.edit),
-                            onPressed: () => editOrder(order['id'], {'status': 'Updated'}),
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () => deleteOrder(order['id']),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+         return StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection('users').snapshots(),
+              builder: (context, userSnapshot) {
+                if (userSnapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                }
+                if (!userSnapshot.hasData || userSnapshot.data!.docs.isEmpty) {
+                return const Center(child: Text('No users found'));
+                }
+                final users = userSnapshot.data!.docs;
+                 return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                    columnSpacing: 56.0,
+                    headingRowHeight: 56.0,
+                    columns: const <DataColumn>[
+                      DataColumn(label: Text('User Name', style: TextStyle(
+                                fontSize: 16 ,
+                                fontWeight: FontWeight.bold,
+                              ),)),
+                      DataColumn(label: Text('Order Date',style: TextStyle(
+                                fontSize: 16 ,
+                                fontWeight: FontWeight.bold,
+                              ),)),
+                      DataColumn(label: Text('Total Amount',style: TextStyle(
+                                fontSize: 16 ,
+                                fontWeight: FontWeight.bold,
+                              ),)),
+                      
+                    
+                    ],
+                    rows: orders.map((order) {
+                      final userId = order.get('userId');
+                      final user = users.firstWhere((x) => x.id == userId);
+                      return DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text(user['username'])),
+                          DataCell(Text(order['timestamp'].toDate().toString())),
+                          DataCell(Text(order['totalAmount'].toString())),
+                          
+                        
+                        ]
+                      );
+                    }).toList(),
+                  ),
                 );
-              }).toList(),
-            ),
-          );
+              },
+            );
+
+         
         },
       ),
     );
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -23,55 +23,118 @@ class CartScreen extends StatelessWidget {
           final cartItems = snapshot.data ?? [];
           if (cartItems.isEmpty) {
             return Center(child: Text('No items in cart'));
-          }
+            }
+            double totalAmount = 0;
+            cartItems.forEach((item) {
+            final data = item.data() as Map<String, dynamic>?;
+            if (data != null && data['price'] != null && data['quantity'] != null) {
+             final price = double.tryParse(data['price'].toString()) ?? 0.0;
+             final quantity = data['quantity'] is int ? data['quantity'] : int.tryParse(data['quantity'].toString()) ?? 0;
+             totalAmount += (price * quantity);
 
-          return ListView.builder(
-            itemCount: cartItems.length,
-            itemBuilder: (context, index) {
-              final item = cartItems[index];
-              final data = item.data() as Map<String, dynamic>?;
+            }
+          });
+          
 
-              // Check if data is valid
-              if (data == null) {
-                return ListTile(
-                  title: Text('Invalid data'),
-                  subtitle: Text('Missing required fields'),
-                );
-              }
+           return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: cartItems.length,
+                  itemBuilder: (context, index) {
+                    final item = cartItems[index];
+                    final data = item.data() as Map<String, dynamic>?;
+                    if (data == null) {
+                      return ListTile(
+                        title: Text('Invalid data'),
+                        subtitle: Text('Missing required fields'),
+                      );
+                    }
 
-              final productId = data['productId']; 
-              final name = data['name'] ?? 'Unknown product';
-              final price = data['price'] ?? 'Unknown price';
-              final quantity = data['quantity'];
-              final image = base64Decode(data['image'] ?? '');
+                    final productId = data['productId'];
+                    final name = data['name'] ?? 'Unknown product';
+                    final price = data['price'] ?? 'Unknown price';
+                    final quantity = data['quantity'];
+                    final image = base64Decode(data['image'] ?? '');
 
-              return ListTile(
-                title: Text(name),
-                subtitle: Text('$price\nQuantity: $quantity'),
-                leading: image.isNotEmpty
-                    ? Image.memory(image, width: 50, height: 50, fit: BoxFit.cover)
-                    : Icon(Icons.image, size: 50),
-                onTap: () {
-                  // Navigate to product details screen
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ProductDetailsScreen(productId: productId),
-                    ),
-                  );
-                },
-                trailing: IconButton(
-                  icon: Icon(Icons.remove_circle_outline),
-                  onPressed: () {
-                    FirestoreService().removeItemFromCart(item.id); // Remove item using FirestoreService
+                    return ListTile(
+                      title: Text(name),
+                      subtitle: Text('Price: \ ${price} DT \nQuantity: $quantity'),
+                      leading: image.isNotEmpty
+                          ? Image.memory(image, width: 50, height: 50, fit: BoxFit.cover)
+                          : Icon(Icons.image, size: 50),
+                      onTap: () {
+                        // Navigate to product details screen
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProductDetailsScreen(productId: productId),
+                          ),
+                        );
+                      },
+                      trailing: IconButton(
+                        icon: Icon(Icons.remove_circle_outline),
+                        onPressed: () {
+                          FirestoreService().removeItemFromCart(item.id); // Remove item using FirestoreService
+                        },
+                      ),
+                    );
                   },
                 ),
-              );
-            },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text('Total Amount: \ ${totalAmount.toStringAsFixed(2) }DT',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () => _confirmOrder(cartItems, totalAmount, context),
+                      child: Text('Confirm Order'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           );
         },
       ),
     );
   }
-}
 
+  void _confirmOrder(
+      List<DocumentSnapshot> cartItems, double totalAmount, BuildContext context) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please log in to confirm your order.')),
+      );
+      return;
+    }
+
+    final orderData = {
+      'userId': user.uid,
+      'totalAmount': totalAmount,
+      'timestamp': FieldValue.serverTimestamp(),
+    };
+
+    try {
+      await FirebaseFirestore.instance.collection('orders').add(orderData);
+
+      // Clear the cart after confirming the order
+      for (var item in cartItems) {
+        await FirestoreService().removeItemFromCart(item.id);
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Order confirmed successfully!')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to confirm order: $e')),
+      );
+    }
+  }
+}
